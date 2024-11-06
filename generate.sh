2ec4c241fa1324ce8f4bfffc6728905c2bd244bc
lvhan028 <lvhan_028@163.com>
2024-11-06 19:47:39 +0800

    add build script and release workflow

--- generate.sh
+++ generate.sh
@@ -1,12 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+WORKSPACE_PATH=$(dirname "$(readlink -f "$0")")
 
-cmake .. \
+builder="-G Ninja"
+
+if [ "$1" == "make" ]; then
+    builder=""
+fi
+
+cmake ${builder} .. \
     -DCMAKE_BUILD_TYPE=RelWithDebInfo \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
     -DCMAKE_CUDA_FLAGS="-lineinfo" \
     -DUSE_NVTX=ON \
-    -DBUILD_TEST=ON \
     -DFETCHCONTENT_UPDATES_DISCONNECTED=ON \
     -DLMDEPLOY_ASAN_ENABLE=OFF \
-    -DLMDEPLOY_UBSAN_ENABLE=OFF \
-    -DCMAKE_CUDA_ARCHITECTURES="80-real"
+    -DLMDEPLOY_UBSAN_ENABLE=OFF
